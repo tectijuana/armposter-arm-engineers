cda463c3d2af0bf7921fe6c7a275492f7cefc0dc
Rodolfo Zamora Lopez <49326574+RodolfoZamora999@users.noreply.github.com>
2020-11-04 09:24:40 -0800

    Update if_assembler.c

--- FileCode/if_assembler.c
+++ FileCode/if_assembler.c
@@ -1,3 +1,19 @@
+/*Instituto Tecnologico de Tijuana
+
+Depto de Sistemas y Computación
+
+Ing. En Sistemas Computacionales
+
+Autor: Rodolfo Zamora Lopez @nickname RodolfoZamora999
+
+Repositorio: https://github.com/tectijuana/armposter-arm-engineers (Respositorio publico)
+
+Fecha de revisión:  03/11/2020
+
+Objetivo del programa:
+Comparación de dos números, se determina cual es mayor
+*/
+
 int main() {
    int max = 0;
    int a = 2;
