46bbba282d16cfe32a14198293ba680ab1cd9ec2
Rodolfo Zamora Lopez <49326574+RodolfoZamora999@users.noreply.github.com>
2020-11-04 09:28:58 -0800

    Update loop_assembler.c

--- FileCode/loop_assembler.c
+++ FileCode/loop_assembler.c
@@ -1,3 +1,14 @@
+/*
+Instituto Tecnologico de Tijuana
+Depto de Sistemas y Computación
+Ing. En Sistemas Computacionales
+Autor: Rodolfo Zamora Lopez @nickname RodolfoZamora999
+Repositorio: https://github.com/tectijuana/armposter-arm-engineers (Respositorio publico)
+Fecha de revisión:  03/11/2020
+Objetivo del programa:
+Bucle que aumenta en 1 una variable hasta cuando se deje de cumplir la condición y se rompa el bucle.
+*/
+
 int main() {
    int a = 0;
    while(a <= 4) {
